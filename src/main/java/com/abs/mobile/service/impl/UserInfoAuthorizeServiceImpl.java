9a65c5e912815b05e83b552c1b685cd1e16632a0
liuhaos2000 <liuhaos2000@163.com>
2015-10-13 15:58:30 +0800

    aasdds

--- src/main/java/com/abs/mobile/service/impl/UserInfoAuthorizeServiceImpl.java
+++ src/main/java/com/abs/mobile/service/impl/UserInfoAuthorizeServiceImpl.java
@@ -1,9 +1,11 @@
 package com.abs.mobile.service.impl;
 
+
 import java.util.Date;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +13,6 @@ import com.abs.mobile.dao.TUserMapper;
 import com.abs.mobile.domain.TUser;
 import com.abs.mobile.service.SessionService;
 import com.abs.mobile.service.UserInfoAuthorizeService;
-import com.abs.util.commom.AbsConst;
 import com.abs.weixin.pojo.OpenId;
 import com.abs.weixin.pojo.RefreshToken;
 import com.abs.weixin.pojo.WeixinUserInfo;
@@ -34,8 +35,6 @@ public class UserInfoAuthorizeServiceImpl implements UserInfoAuthorizeService {
 
 		// 取得OpenId
 		OpenId openId = WeiXinIFUtil.getOpenId(code);
-		// 拉取用户信息 TODO
-		WeixinUserInfo wxUserInfo = WeiXinIFUtil.getUserinfo(openId.getAccess_token(), openId.getOpenid());
 		// 获取系统时间
 		Date date = new Date();
 		// 取得数据库的用户信息
@@ -44,16 +43,7 @@ public class UserInfoAuthorizeServiceImpl implements UserInfoAuthorizeService {
 		if (tUser == null) {
 			tUser = new TUser();
 			tUser.setOpenId(openId.getOpenid());
-            if(wxUserInfo!=null){
-                tUser.setNickname(wxUserInfo.getNickname());
-                tUser.setWeixinSex(wxUserInfo.getSex());
-                tUser.setCity(wxUserInfo.getCity());
-                tUser.setProvince(wxUserInfo.getProvince());
-                tUser.setCountry(wxUserInfo.getCountry());
-                tUser.setWeixinImageUrl(wxUserInfo.getHeadimgurl());
-            }
             // TODO TOKEN
-            
 			tUser.setJifen(0);
 			tUser.setDelFlg("0");
 			tUser.setcDate(date);
@@ -63,6 +53,24 @@ public class UserInfoAuthorizeServiceImpl implements UserInfoAuthorizeService {
 			// 插入数据库
 			tUserMapper.insert(tUser);
         } else {
+            
+            if(StringUtils.isNotEmpty(tUser.getNickname())){
+                //已经有用户信息
+                //启动多线程更新用户信息
+                Thread thread = new Thread(){
+                    public void run(){
+                      System.out.println("Thread Running");
+                    }
+                 };
+                 thread.start();
+            }else{
+                //还没有用户信息
+                //不启动多线程序
+            }
+            
+            // 拉取用户信息 TODO
+            WeixinUserInfo wxUserInfo = WeiXinIFUtil.getUserinfo(
+                    openId.getAccess_token(), openId.getOpenid());
             // 每次登录时，更新用户信息
             if(wxUserInfo!=null){
                 tUser.setNickname(wxUserInfo.getNickname());
