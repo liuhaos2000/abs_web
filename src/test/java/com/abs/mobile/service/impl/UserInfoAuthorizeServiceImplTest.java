f539cb199c9ade46186e555043532f6573fcc71d
liuhaos2000 <liuhaos2000@163.com>
2017-10-31 21:08:56 +0800

    啊啊啊

--- src/test/java/com/abs/mobile/service/impl/UserInfoAuthorizeServiceImplTest.java
+++ src/test/java/com/abs/mobile/service/impl/UserInfoAuthorizeServiceImplTest.java
@@ -1,9 +1,19 @@
 package com.abs.mobile.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class UserInfoAuthorizeServiceImplTest {
 
-
+/**
+ * 您好，您已购买成功。
+*
+*商品信息：{{name.DATA}}
+*{{remark.DATA}}
+ * @param args
+ */
 	public static void main(String[] args) {
+	
 	}
-		
 	}
